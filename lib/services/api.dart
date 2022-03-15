4ef602fb0f4f17ea99c4f32df83d3b5f963edca3
Dimitri Lefebvre <64543724+Antharas62@users.noreply.github.com>
2022-03-15 10:37:40 +0100

    11 - classe API

---
+++ lib/services/api.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_netflix/services/api_key.dart';
+
+class API{
+  final String apikey = APIKey.apikey;
+  final String baseURL = "https://developers.themoviedb.org/3";
+  final String baseImageURL = "https://image.tmdb.org/t/p/w500";
+  final String baseVideoURL = "https://www.youtube.com/watch?v=";
+}
