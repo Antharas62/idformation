1c57d3e9a59b90cdd7f826662064f10c78ae80bb
Dimitri Lefebvre <64543724+Antharas62@users.noreply.github.com>
2022-03-16 10:36:06 +0100

    23 - Maj Movie class

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_netflix/models/person.dart';
 import 'package:flutter_netflix/services/api.dart';
 
 class Movie {
@@ -5,24 +6,47 @@ class Movie {
   final String name;
   final String description;
   final String? posterPath;
+  final List<String>? genres;
+  final String? releaseDate;
+  final double? vote;
+  final List<String>? videos;
+  final List<Person>? casting;
+  final List<String>? images;
   Movie({
     required this.id,
     required this.name,
     required this.description,
     this.posterPath,
+    this.genres,
+    this.releaseDate,
+    this.vote,
+    this.videos,
+    this.casting,
+    this.images,
   });
 
-  Movie copyWith({
-    int? id,
-    String? name,
-    String? description,
-    String? posterPath,
-  }) {
+  Movie copyWith(
+      {int? id,
+      String? name,
+      String? description,
+      String? posterPath,
+      List<String>? genres,
+      String? releaseDate,
+      double? vote,
+      List<String>? videos,
+      List<Person>? casting,
+      List<String>? images}) {
     return Movie(
       id: id ?? this.id,
       name: name ?? this.name,
       description: description ?? this.description,
       posterPath: posterPath ?? this.posterPath,
+      genres: genres ?? this.genres,
+      releaseDate: releaseDate ?? this.releaseDate,
+      vote: vote ?? this.vote,
+      videos: videos ?? this.videos,
+      casting: casting ?? this.casting,
+      images: images ?? this.images,
     );
   }
 
@@ -39,6 +63,18 @@ class Movie {
   String posterURL(){
     API api = API();
     return api.baseImageURL + posterPath!;
+  }
+
+  String reformatGenres() {
+    String categories = '';
+    for (int i = 0; i < genres!.length; i++) {
+      if (i == genres!.length - 1) {
+        categories = categories + genres![i];
+      } else {
+        categories = categories + '${genres![i]}, ';
+      }
+    }
+    return categories;
   }
 
 }
