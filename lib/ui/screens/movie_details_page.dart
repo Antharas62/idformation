d8d70d47c6ca9d5f83d04efda0649fbdcd53f27b
Dimitri Lefebvre <64543724+Antharas62@users.noreply.github.com>
2022-03-16 11:59:02 +0100

    26 - Ajout des bouttons

--- lib/ui/screens/movie_details_page.dart
+++ lib/ui/screens/movie_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix/models/movie.dart';
 import 'package:flutter_netflix/repositories/data_repositories.dart';
+import 'package:flutter_netflix/ui/widgets/action_buttun.dart';
 import 'package:flutter_netflix/ui/widgets/movie_info.dart';
 import 'package:flutter_netflix/utils/constant.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -9,7 +10,7 @@ import 'package:provider/provider.dart';
 
 class MovieDetailsPage extends StatefulWidget {
   final Movie movie;
-  const MovieDetailsPage({ Key? key, required this.movie }) : super(key: key);
+  const MovieDetailsPage({Key? key, required this.movie}) : super(key: key);
 
   @override
   State<MovieDetailsPage> createState() => _MovieDetailsPageState();
@@ -36,23 +37,37 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBackgroundColor,
-      appBar: AppBar(
         backgroundColor: kBackgroundColor,
-      ),
-      body: (newMovie == null)?
-      Center(
-        child: SpinKitFadingCircle(color: kPrimarycolor, size: 20),
-      ): ListView(
-        children: [
-          Container(
-            height: 220,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.red,
-          ),
-          MovieInfo(movie: newMovie!),
-        ],
-      )
-    );
+        appBar: AppBar(
+          backgroundColor: kBackgroundColor,
+        ),
+        body: (newMovie == null)
+            ? Center(
+                child: SpinKitFadingCircle(color: kPrimarycolor, size: 20),
+              )
+            : ListView(
+                children: [
+                  Container(
+                    height: 220,
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.red,
+                  ),
+                  MovieInfo(movie: newMovie!),
+                  const SizedBox(height: 10),
+                  ActionButtun(
+                    label: "Lecture",
+                    icon: Icons.play_arrow,
+                    bgColor: Colors.white,
+                    color: kBackgroundColor,
+                  ),
+                  const SizedBox(height: 10),
+                  ActionButtun(
+                    label: "Télécharger la vidéo",
+                    icon: Icons.download,
+                    bgColor: Colors.grey.withOpacity(0.3),
+                    color: Colors.white,
+                  ),
+                ],
+              ));
   }
 }
